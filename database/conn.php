7d0dca46e88ca1365a6bf071954784f8e7952e99
superdoggo83 <bramvandersluis83@gmail.com>
2025-03-06 10:23:55 +0100

    updated conn.php
    updated the conn.php file to use mysqli instead of pdo to be compatible with the signup code

--- database/conn.php
+++ database/conn.php
@@ -5,14 +5,12 @@ $username = 'root';
 $password = '';
 $dbname = 'zwollemapproject';
 
-#try catch to view if the connection was established correctly
-try{
+
 # variable for establishing the connection with the database using the credentials we made earlier
-$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-#setattribute to enable error reporting 
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch(PDOException $e){
-    echo "connection failed: ". $e->getMessage(); 
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if($conn ->connect_errno){
+    echo "failed to connect to mysql: " . $conn ->connect_error;
+    exit();
 }
 ?>
