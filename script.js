6c3fbddd5eb98c4b18839313482da4528a6a0b81
Nikki Nip <145456210+nikkinip@users.noreply.github.com>
2025-03-10 13:12:00 +0100

    homepage

---
+++ script.js
@@ -0,0 +1,5 @@
+
+        document.getElementById("scroll-down").addEventListener("click", function() {
+            document.getElementById("map-section").scrollIntoView({ behavior: "smooth" });
+        });
+   
