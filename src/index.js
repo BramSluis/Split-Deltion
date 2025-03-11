2e8df6cc34db9d81afd2dca0d099469b1087fb71
Nikki Nip <145456210+nikkinip@users.noreply.github.com>
2025-03-11 18:07:22 +0100

    finally the map is on the page.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Gebruik react-dom/client
+
+import Map from './Map';
+const root = ReactDOM.createRoot(document.getElementById("map"));
+root.render(
+        
+    
+        <Map></Map>
+    
+);
